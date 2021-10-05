77fb474d3250fb7b08bb8c1e2e9835fecc78fc0e
Kelvin Alves <kelvinpalves@gmail.com>
2021-10-04 22:39:42 -0300

    fix null values in timeseries list.

--- src/main/java/br/com/forgeit/urbanobservatory/infra/client/model/TimeSerie.java
+++ src/main/java/br/com/forgeit/urbanobservatory/infra/client/model/TimeSerie.java
@@ -1,9 +1,11 @@
 package br.com.forgeit.urbanobservatory.infra.client.model;
 
+import lombok.Data;
 import lombok.ToString;
 
 import java.util.Map;
 
+@Data
 @ToString
 public class TimeSerie {
     private Map<String, String> unit;
